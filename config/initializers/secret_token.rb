aa5201f9dbd7715efae68533168138838e70c8d0
Fatimah <ojo-sama-3000@hotmail.com>
2014-07-20 13:41:31 +0300

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Test22::Application.config.secret_key_base = '5e6ff7457aa79e09098ed8cb7c70ca0324ba5bb0e3bb3737891d8b84eb33cb2b8913c1a6dd61811ccf58eabca07c54f574d839485091545fa74728cd641022c7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Test22::Application.config.session_store :cookie_store, key: '_test22_session'
